88ca70cc151c7cf050aec3ba26a35e874d0e1f13
Myleshen Praburam <mylesuge@gmail.com>
2020-11-05 00:27:05 +0530

    Added mongo template

--- db_connection/db_mongo.py
+++ db_connection/db_mongo.py
@@ -1,1 +1,18 @@
 import pymongo
+
+
+class Db:
+    def __init__(self) -> None:
+        self.collection = self.connection()
+
+    def connection(self):
+        connection = pymongo.MongoClient("mongodb://localhost:27017")
+        database = connection.get_database("anime_tracker")
+        return database.get_collection("testing")
+
+    def add_entry(self, entry):
+        try:
+            self.collection.insert_one(entry)
+            return "Added data successfully"
+        except:
+            return "Failed to add the data"
