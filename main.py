8897ca244bb2b74546a1d942d7047175c7b80add
Myleshen Praburam <mylesuge@gmail.com>
2020-11-08 00:49:44 +0530

    ProcFile Changes, uvicorn command changes, python version to 3.9

--- main.py
+++ main.py
@@ -26,4 +26,4 @@ async def get_short_url(q: str):
 
 
 if __name__ == "__main__":
-    uvicorn.run("server.app:app", host="0.0.0.0", port=8000, reload=True)
+    uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=True)
